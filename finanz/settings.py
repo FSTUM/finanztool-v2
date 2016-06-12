4a81031e09a85c43f83a8a6235be75a5a46a8886
Ellen Maeckelburg <maeckelburg@fs.tum.de>
2016-06-12 23:58:55 +0200

    Now with fold marks

--- finanz/settings.py
+++ finanz/settings.py
@@ -114,9 +114,14 @@ TIME_ZONE = 'Europe/Berlin'
 
 USE_I18N = True
 
-USE_L10N = True
+USE_L10N = False
 
 USE_TZ = True
+
+DATE_FORMAT = "d.m.Y"
+DATETIME_FORMAT = "d.m.Y"
+SHORT_DATE_FORMAT = "d.m.Y"
+SHORT_DATETIME_FORMAT = "d.m.Y"
 
 
 # Static files (CSS, JavaScript, Images)
