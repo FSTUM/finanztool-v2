328659d4c62ea108e5b79e0012e35287422b3e0b
Ellen Maeckelburg <maeckelburg@fs.tum.de>
2016-10-26 00:01:56 +0200

    More views for Aufgaben and can now easily be marked as done

--- aufgaben/urls.py
+++ aufgaben/urls.py
@@ -11,6 +11,20 @@ urlpatterns = [
     # ex: /aufgaben/unerledigt
     url(r'^$', views.unerledigt, name='unerledigt'),
 
+    # ex: /aufgaben/neu/
+    url(r'^neu/$', views.form_aufgabe, name='neu'),
+
+    # ex: /aufgaben/4/aendern/
+    url(r'^(?P<aufgabe_id>[0-9]+)/aendern/$',
+        views.form_aufgabe, name='aendern'),
+
+    # ex: /aufgaben/art/neu/
+    url(r'^art/neu/$', views.form_aufgabenart, name='neu_aufgabenart'),
+
+    # ex: /aufgaben/art/4/aendern/
+    url(r'^/art/(?P<aufgabenart_id>[0-9]+)/aendern/$',
+        views.form_aufgabenart, name='aendern_aufgabenart'),
+
     # ex: /aufgaben/4/
     url(r'^(?P<aufgabe_id>[0-9]+)/$', views.aufgabe, name='aufgabe'),
 
--- aufgaben/views.py
+++ aufgaben/views.py
@@ -2,13 +2,14 @@ from django.contrib.auth.decorators import login_required
 from django.shortcuts import get_object_or_404, render, redirect
 
 from .models import Aufgabe, Aufgabenart
+from .forms import AufgabeForm, AufgabenartForm, AufgabeErledigtForm
 
 
 @login_required
 def unerledigt(request):
-    aufgaben = Aufgabe.objects.filter(erledigt=False)
+    aufgaben = Aufgabe.objects.filter(erledigt=False).order_by('-frist')
     meine_aufgaben = Aufgabe.objects.filter(erledigt=False,
-                                            zustaendig=request.user)
+                                            zustaendig=request.user).order_by('-frist')
     context = {
             'aufgaben': aufgaben,
             'meine_aufgaben': meine_aufgaben,
@@ -17,8 +18,55 @@ def unerledigt(request):
 
 
 @login_required
+def form_aufgabe(request, aufgabe_id=None):
+    aufgabe = None
+    if aufgabe_id:
+        aufgabe = get_object_or_404(Aufgabe, pk=aufgabe_id)
+
+    if request.method == "POST":
+        form = AufgabeForm(request.POST, instance=aufgabe)
+
+        if form.is_valid():
+            aufgabe = form.save()
+            return redirect('aufgaben:aufgabe', aufgabe_id=aufgabe.pk)
+    else:
+        form = AufgabeForm(initial={'zustaendig': request.user},
+                           instance=aufgabe)
+
+    context = {
+            'form': form,
+            'aufgabe': aufgabe
+            }
+
+    return render(request, 'aufgaben/form_aufgabe.html', context)
+
+
+@login_required
+def form_aufgabenart(request, aufgabenart_id=None):
+    aufgabenart = None
+    if aufgabenart_id:
+        aufgabenart = get_object_or_404(Aufgabenart, pk=aufgabenart_id)
+
+    if request.method == "POST":
+        form = AufgabenartForm(request.POST, instance=aufgabenart)
+
+        if form.is_valid():
+            aufgabenart = form.save()
+            return redirect('aufgaben:neu')
+    else:
+        form = AufgabenartForm()
+
+    context = {
+            'form': form,
+            'aufgabenart': aufgabenart
+            }
+
+    return render(request, 'aufgaben/form_aufgabenart.html', context)
+
+
+@login_required
 def alle(request):
-    alle = Aufgabe.objects.all()
+    alle = Aufgabe.objects.all().order_by('-frist')
     context = {
             'alle': alle,
             }
@@ -29,7 +77,16 @@ def alle(request):
 def aufgabe(request, aufgabe_id):
     aufgabe = get_object_or_404(Aufgabe, pk=aufgabe_id)
 
+    form = AufgabeErledigtForm(request.POST or None)
+    if request.method == 'POST':
+        if 'erledigt' in request.POST:
+            if form.is_valid():
+                aufgabe.erledigt = True
+                aufgabe.save()
+                return redirect('aufgaben:aufgabe', aufgabe_id=aufgabe.pk)
+
     context = {
             'aufgabe': aufgabe,
+            'form': form,
             }
     return render(request, 'aufgaben/aufgabe.html', context)
