bab984fb24e0beab9783454527074ff04c8910a0
Frank Elsinga <frank@elsinga.de>
2021-07-05 00:24:06 +0200

    reordered settings.MIDDLEWARE according to django docs
    Signed-off-by: Frank Elsinga <frank@elsinga.de>

--- finanz/settings.py
+++ finanz/settings.py
@@ -41,6 +41,7 @@ INSTALLED_APPS = [
 ]
 
 MIDDLEWARE = [
+    "django.middleware.security.SecurityMiddleware",
     "django.contrib.sessions.middleware.SessionMiddleware",
     "django.middleware.locale.LocaleMiddleware",
     "django.middleware.common.CommonMiddleware",
@@ -48,7 +49,6 @@ MIDDLEWARE = [
     "django.contrib.auth.middleware.AuthenticationMiddleware",
     "django.contrib.messages.middleware.MessageMiddleware",
     "django.middleware.clickjacking.XFrameOptionsMiddleware",
-    "django.middleware.security.SecurityMiddleware",
 ]
 
 ROOT_URLCONF = "finanz.urls"
