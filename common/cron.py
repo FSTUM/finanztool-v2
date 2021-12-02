3d7a5dfc7cf45edb94d53cd39eccb3bc0c0c4f26
Frank Elsinga <frank@elsinga.de>
2021-12-02 23:12:38 +0100

    minor formatting changes

--- common/cron.py
+++ common/cron.py
@@ -1,6 +1,7 @@
 from typing import List
 
 from django.contrib.auth.models import User  # pylint: disable=imported-auth-user
+from django.db.models import QuerySet
 
 from aufgaben.models import Aufgabe
 from rechnung.models import Rechnung
@@ -11,9 +12,9 @@ from .models import Mail, Settings
 def ueberfaellige_rechnung_reminder() -> None:
     settings: Settings = Settings.load()
     if settings and settings.ueberfaellige_rechnung_mail:
-        rechnung: Rechnung
-        rechnungen: List[Rechnung] = [rechnung for rechnung in Rechnung.objects.all() if rechnung.faellig()]
-        settings.ueberfaellige_rechnung_mail.send_mail({"rechnungen": rechnungen}, Mail.FINANZ)
+        rechnungen_qs: QuerySet[Rechnung] = Rechnung.objects.all()
+        faellige_rechnungen: List[Rechnung] = [rechnung for rechnung in rechnungen_qs if rechnung.faellig()]
+        settings.ueberfaellige_rechnung_mail.send_mail({"rechnungen": faellige_rechnungen}, Mail.FINANZ)
 
 
 def zugewiesene_aufgabe_reminder() -> None:
